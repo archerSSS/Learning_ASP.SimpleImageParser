27a4fc9fe867093d0f920325683ad1f9fafeda9e
archyrysss <archyrysss@gmail.com>
2021-03-10 15:59:02 +0300

    Добавьте файлы проекта.

---
+++ SimpleImageParser/Pages/MainPage.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainPage.aspx.cs" Inherits="SimpleImageParser.Pages.MainPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" Text="Put url of html you want to parse images from"></asp:Label>
+            <div></div>
+            <asp:TextBox runat="server" ID="TextUrl" Width="500"></asp:TextBox>
+            <div></div>
+            <asp:Button runat="server" Text="Parse Images" OnClick="Unnamed_Click"/>
+            <asp:Table runat="server" ID="aspElements">
+                
+            </asp:Table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SimpleImageParser/Pages/MainPage.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleImageParser.Pages
+{
+    public partial class MainPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void ParseImages()
+        {
+            string url = TextUrl.Text;
+
+            try
+            {
+                using (HttpClientHandler handler = new HttpClientHandler() { AllowAutoRedirect = false, AutomaticDecompression = System.Net.DecompressionMethods.Deflate | System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.None })
+                {
+                    using (var client = new HttpClient(handler))
+                    {
+                        using (HttpResponseMessage response = client.GetAsync(url).Result)
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                var html = response.Content.ReadAsStringAsync().Result;
+                                if (!string.IsNullOrEmpty(html))
+                                {
+                                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                                    doc.LoadHtml(html);
+
+                                    var imgs_url = doc.DocumentNode.Descendants("img");
+
+                                    aspElements.Controls.Add(new TableRow());
+
+                                    if (imgs_url != null)
+                                    {
+                                        var mm = imgs_url.Select(el => el.GetAttributeValue("src", null));
+                                        if (mm == null) return; 
+                                        foreach (string s in mm)
+                                        {
+                                            if (!string.IsNullOrEmpty(s))
+                                            {
+                                                if (aspElements.Rows[aspElements.Rows.Count - 1].Cells.Count > 4)
+                                                {
+                                                    aspElements.Controls.Add(new TableRow());
+                                                }
+
+                                                TableRow tr = aspElements.Rows[aspElements.Rows.Count - 1];
+
+                                                Image i = new Image();
+                                                i.ImageUrl = s;
+
+                                                TableCell tc = new TableCell();
+                                                tc.Controls.Add(i);
+                                                tr.Controls.Add(tc);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+
+            }
+        }
+
+        protected void Unnamed_Click(object sender, EventArgs e)
+        {
+            ParseImages();
+        }
+    }
+}
